d18d1e5483beb216344e6f089c667fd09755b153
frowhy <frowhy@gmail.com>
2017-03-16 18:58:14 +0800

    …

--- app/src/main/java/com/frowhy/dominate/MainActivity.java
+++ app/src/main/java/com/frowhy/dominate/MainActivity.java
@@ -3,12 +3,13 @@ package com.frowhy.dominate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 import ezy.assist.compat.SettingsCompat;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     private Intent mPpwButtonService;
     private Button mBtnOpenPopupWindow;
     private boolean mIsOpen;
